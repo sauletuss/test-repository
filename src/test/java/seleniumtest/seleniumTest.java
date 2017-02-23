dffac9ba2dd7d8c0dad08c4236d54a32a92d1148
Saule Tussupbekova <saule_tus@mail.ru>
2017-02-23 23:08:18 +0600

    Задача №3

--- src/test/java/seleniumtest/seleniumTest.java
+++ src/test/java/seleniumtest/seleniumTest.java
@@ -1,19 +1,32 @@
 package seleniumtest;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class seleniumTest {
-
+	
+	private WebDriver driver;
+	
+	@Before
+	public void start() {
+		driver = new FirefoxDriver();
+	}
+	
 	@Test
 	public void test() {
-		WebDriver driver;
-		driver = new FirefoxDriver();
-		
-		driver.get("http://google.com/");
-		
+		driver.get("http://localhost/litecart/admin/");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name("login")).click();
+	}
+	
+	@After
+	public void stop() {
 		driver.quit();
 	}
 
